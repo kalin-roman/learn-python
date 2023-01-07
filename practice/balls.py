33cebb7e6b6e8f71436b5f1b48e16a7d6bad1815
Roman Kalin <kalin.roman.m@gmail.com>
2023-01-08 01:05:59 +0200

    hell

--- practice/balls.py
+++ practice/balls.py
@@ -1,4 +1,4 @@
-import  pygame
+from pygame import Rect
 from random import randint
 from pygame.draw import *
 from pygame.surface import Surface
@@ -26,21 +26,84 @@ class Vector():
         self.x += v.x
         self.y += v.y
 
-        
-class polygon():
-    point : Vector
-    color : list
-    screen : Surface
-    speed : Vector
-    size : Vector
 
-    def __init__(self,screen) -> None:
+class Ellipse():
+    screen: Surface
+    color: tuple
+    rect: Rect
+    width: int
+    size: Vector
+    point: Vector
+    speed: Vector
 
-        
+    def __init__(self, screen):
+        self.width = randint(20,40)
+        self.color = COLORS[randint(0,len(COLORS)-1)]
+        self.screen = screen
+        self.size = Vector(*self.screen.get_size())
+        radx = self.width//2
+        x = randint(radx, self.size.x- radx*10)
+        y = randint(radx, self.size.y - radx)
+        self.point = Vector(x,y)
+        self.rect = Rect(0,0,self.width,self.width)
+        speedx = randint(8,13)
+        speedy = randint(8,13) 
+        self.speed = Vector(speedx,speedy)
+
+    def draw(self):
+        self.rect.center = self.point.get()
+        ellipse(self.screen, self.color, self.rect)
+
+    
+    def move(self):
+        radx = self.width//2
+        self.point.add(self.speed)
+        if not radx < self.point.x < self.size.x - radx:
+            self.speed.x = -self.speed.x
+        if not radx < self.point.y < self.size.y - radx:
+            self.speed.y = -self.speed.y
+class Rectangl():
+    color: tuple
+    rect: Rect
+    width: int
+    screen: Surface
+    speed: int
+    point: Vector
+    size: Vector
+
+    def __init__(self, screen):
+        self.width = randint(60, 100)
+        self.color = COLORS[randint(0,len(COLORS)-1)]
+        self.screen = screen
+        radx = self.width//2
+        self.size = Vector(*self.screen.get_size())
+        x = randint(radx, self.size.x- radx)
+        y = randint(radx, self.size.y - radx)
+        speedx = randint(1,5)
+        speedy = randint(1,5) 
+        self.speed = Vector(speedx,speedy)
+        self.point = Vector(x,y)
+        self.rect = Rect(0,0, self.width, self.width)
+        #self.rect = [self.cord,self.cord,self.cord,self.cord]
+
+    def draw(self):
+        self.rect.center = self.point.get()
+        rect(self.screen,self.color,self.rect)
+
+
+    def move(self):
+        radx = self.width//2
+        self.point.add(self.speed)
+        if not radx < self.point.x < self.size.x - radx:
+             self.speed.x = -self.speed.x
+        if not radx < self.point.y < self.size.y - radx:
+             self.speed.y = -self.speed.y
+    
 class Ball():
+
     rad: int
     point: Vector
-    color: list
+    color: tuple
     screen: Surface
     speed : Vector
     size : Vector
@@ -68,6 +131,8 @@ class Ball():
             self.speed.x = -self.speed.x
         if not self.rad < self.point.y < self.size.y - self.rad:
             self.speed.y = -self.speed.y
+
+
     def check(self,x,y):
         formule1 = ((self.point.x - x) ** 2 + (self.point.y - y) ** 2) ** 0.5
         return formule1 <= self.rad
--- practice/practic7.py
+++ practice/practic7.py
@@ -9,17 +9,8 @@ size = Vector(1200, 900)
 screen = pygame.display.set_mode(size.get())
 score = 0
 balls = [Ball(screen) for i in range(5)]
-
-def new_ball():
-    global x, y, r
-    x = randint(100,700)
-    y = randint(100,500)
-    r = randint(30,50)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-    
-def click(event):
-    print(x, y, r)
+rectangle = [Rectangl(screen) for i in range(5)]
+elipses = [Ellipse(screen) for i in range(5)]
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -33,16 +24,24 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             cor = event.pos
             flag = False
-            for ball in balls:
-                flag = ball.check(*cor) or flag
+            for fig in balls + rectangle:
+                flag = fig.check(*cor) or flag
             if flag:
                 score += 1
                 print('Click!')
                 # click(event)
                 print(score)
-    for ball in balls:
-        ball.draw()
-        ball.move()
+    for fig in balls:
+        fig.draw()
+        fig.move()
+
+    for figers in rectangle:
+        figers.draw()
+        figers.move()
+
+    for figerses in elipses:
+        figerses.draw()
+        figerses.move()
         
     pygame.display.update()
     screen.fill(BLACK)
